fed74f48efd50bfca4e0cd30a8a673031e42629e
msmaiaa <roxx77344@gmail.com>
2023-03-25 01:42:45 -0300

    better temporary navbar and header styles

--- src/components/header.rs
+++ src/components/header.rs
@@ -6,7 +6,7 @@ pub fn Header(cx: Scope) -> impl IntoView {
     //  TODO: forum logo instead of text
     //  TODO: use custom header background image
     view! {cx,
-        <header id="header" class="bg-header w-full flex justify-center">
+        <header id="header" class="bg-header w-full flex justify-center h-12">
             <div class="flex items-center w-[1100px]">
                 <A href="/" class="text-2xl font-bold text-text_primary">"Leptos"</A>
             </div>
--- src/components/nav.rs
+++ src/components/nav.rs
@@ -6,23 +6,24 @@ use crate::app::LoggedUserData;
 #[component]
 pub fn Navbar(cx: Scope) -> impl IntoView {
     //  TODO: decent dropdown menu like xenforo?
+    //  TODO: make it sticky
     let user_data =
         use_context::<RwSignal<Option<LoggedUserData>>>(cx).expect("user_data context is not set");
 
     view! {cx,
-        <div class="w-full flex justify-center">
-            <div class="w-[1100px]">
+        <div class="w-full flex justify-center h-9">
+            <div class="w-[1100px] flex items-center h-full">
                 {move || {
                     match user_data.get() {
-                        Some(data) => {
-                            let img_url = match data.avatar_url {
+                        Some(user) => {
+                            let img_url = match user.avatar_url {
                                 Some(url) => url,
                                 None => "/assets/default_avatar.png".to_string(),
                             };
                             view! {cx,
-                                    <div>
-                                        <img src=img_url alt="Avatar" height="25" width="25" class="rounded-full"/>
-                                        <p>{data.username}</p>
+                                    <div class="w-full flex justify-end">
+                                        <img src=img_url alt="Avatar" height="25" width="25" class="rounded-full mr-2"/>
+                                        <p>{user.username}</p>
                                     </div>
                             }
                         }
